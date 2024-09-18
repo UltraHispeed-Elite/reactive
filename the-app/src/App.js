413ef27447b69989eafe0f288b0066591b2b3e17
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-09-18 18:47:22 +0000

    rest

--- the-app/src/App.js
+++ the-app/src/App.js
@@ -6,14 +6,25 @@ import Page from './Page.js';
 import { useState, createContext } from 'react';
 
 export const ThemeContext = createContext('light');
+export const ChangeContext = createContext('change');
 
 function App() {
   const [theme, changeTheme] = useState('light');
 
+  function test() {
+    if(theme === 'light') {
+      changeTheme('dark');
+    }else {
+      changeTheme('light');
+    }
+  }
+
   return (
     <div className="App">
       <ThemeContext.Provider value={theme}>
-        <Page />
+        <ChangeContext.Provider value={test}>
+          <Page />
+        </ChangeContext.Provider>
       </ThemeContext.Provider>
     </div>
   );
--- the-app/src/Page.js
+++ the-app/src/Page.js
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
 
-import { ThemeContext } from './App.js';
+import { ThemeContext, ChangeContext } from './App.js';
 
 export default function Page() {
     const theme = useContext(ThemeContext);
+    const change = useContext(ChangeContext);
 
     return(
-        <div>
+        <div onClick={change}>
             <h1>Hello {theme}</h1>
         </div>
     )
